21c8d1f7dd9660b5004bb8d09472b164752f6bb0
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-09-05 20:48:47 +0600

    update

---
+++ variable/variable.js
@@ -0,0 +1,11 @@
+const a = 4;
+console.log(a);
+var x =30;
+var y ="hello world";
+document.write(x);
+document.write(y);
+if (X<=30){
+    document.write("true");
+}else{
+    document.write("false");
+}
