afadcfe3ec76944e134afd2bb24730b156f5ab07
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-09-05 21:13:49 +0600

    Update variable.js

--- variable/variable.js
+++ variable/variable.js
@@ -1,11 +1,24 @@
-const a = 4;
-console.log(a);
-var x =30;
-var y ="hello world";
-document.write(x);
-document.write(y);
-if (X<=30){
-    document.write("true");
-}else{
-    document.write("false");
+// const a = 4;
+// console.log(a);
+// var x =30;
+// var y ="hello world";
+// document.write(x);
+// document.write(y);
+// if (x>30){
+//     document.write("true");
+// }else{
+//     document.write("false");
+// }
+// Global Scope 
+var global = "hello ashikuzzaman";
+function  ashik (){
+    console.log (global);
 }
+console.log(global);
+function local (){
+    var local = "hello local";
+    console.log(local);
+}
+local();
+// console.log(local); // error
+// block scope
